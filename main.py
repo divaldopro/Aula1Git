dc28e6f39422dd033e909333ef8a710753ec0035
Divaldo William Marques <divaldopro@gmail.com>
2024-08-27 11:13:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/delloword")
 
 # 127.0.0.1:8000/
 async def root():
     return {"message": "Hello World"}
 
 # 127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo"}
 
-# 127.0.0.1:8000/teste2
-@app.get("/teste2")
-async def funcaoteste():
-    return {"teste2": "deu certo o segundo"}
